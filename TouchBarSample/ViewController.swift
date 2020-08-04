9583e966f67e4b2745492de3e360d2369f686563
Koichiro Eto <koichiro.eto@gmail.com>
2020-08-05 08:52:44 +0900

    Show a message to the touch bar.

--- TouchBarSample/ViewController.swift
+++ TouchBarSample/ViewController.swift
@@ -8,20 +8,41 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class WindowController: NSWindowController {
+    override func windowDidLoad() {
+        super.windowDidLoad()
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    override func makeTouchBar() -> NSTouchBar? {
+        let touchBarIdenitifier = NSTouchBar.CustomizationIdentifier("com.eto.TestToolBar")
+        let touchBarLabelIdentifier = NSTouchBarItem.Identifier(rawValue: "com.eto.TestToolBar.label")
+        let touchBar = NSTouchBar()
+        touchBar.delegate = self
+        touchBar.customizationIdentifier = touchBarIdenitifier
+        touchBar.defaultItemIdentifiers = [touchBarLabelIdentifier, .fixedSpaceLarge, .otherItemsProxy]
+        touchBar.customizationAllowedItemIdentifiers = [touchBarLabelIdentifier]
+        return touchBar
     }
-
-
 }
 
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
+
+extension WindowController: NSTouchBarDelegate {
+    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
+        if identifier.rawValue == "com.eto.TestToolBar.label" {
+            let custom = NSCustomTouchBarItem(identifier: identifier)
+            custom.customizationLabel = "Label"
+            let label = NSTextField.init(labelWithString: "⭐️")
+            custom.view = label
+            return custom
+        }
+        return nil
+    }
+}
