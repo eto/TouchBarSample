17ce2cce495026cb1fc63e5209917040c56f0e0b
Koichiro Eto <koichiro.eto@gmail.com>
2020-08-05 08:43:10 +0900

    Initial Commit

---
+++ TouchBarSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarSample
+//
+//  Created by Koichiro Eto on 2020/08/05.
+//  Copyright © 2020 Koichiro Eto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarSample
+//
+//  Created by Koichiro Eto on 2020/08/05.
+//  Copyright © 2020 Koichiro Eto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
